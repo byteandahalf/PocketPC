c29869c8b7e6a0ede3448f18a9bdfcd581cecee5
RedstoneGunMade <RedstoneGunMade@users.noreply.github.com>
2015-08-28 18:14:36 +0100

    Added Beacon Tile

---
+++ jni/PocketPC/tile/BeaconTile.cpp
@@ -0,0 +1,21 @@
+#include <BrewingStandTile.h>
+
+BeaconTile::BeaconTile(int id) : EntityTile(id, &Material::metal) {
+	tex = getTextureUVCoordinateSet("glass", 0);
+	secondary_tex = getTextureUVCoordinateSet("obsidian", 0);
+	terciary_tex = getTextureUVCoordinateSet("beacon", 0);
+	//fourth_tex = getTextureUVCoordinateSet("beacon_beam", 0);
+	//fifth_tex = getTextureUVCoordinateSet("beacon_beam", 1);
+	renderPass = Tile::glass->renderPass;
+	Tile::solid[id] = false;
+	Tile::lightBlock[id] = 0;
+	new TileItem(id - 256);
+}
+
+const TextureUVCoordinateSet& BrewingStandTile::getTexture(signed char side, int data) {
+	return side == 0 ? tex : (side == 1 ? secondary_tex : terciary_tex);
+}
+
+TileEntity* BeaconTile::newTileEntity(const TilePos& tp) {
+	return new BeaconTileEntity();
+}
