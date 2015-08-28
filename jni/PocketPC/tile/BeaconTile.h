5d02fa198f91d9141e031b57000d4edb5f3917cc
RedstoneGunMade <RedstoneGunMade@users.noreply.github.com>
2015-08-28 18:44:53 +0100

    Update BeaconTile.h

--- jni/PocketPC/tile/BeaconTile.h
+++ jni/PocketPC/tile/BeaconTile.h
@@ -3,15 +3,18 @@
 #include "mcpe/tile/EntityTile.h"
 #include "mcpe/item/Item.h"
 #include "mcpe/util/Random.h"
-#include "mcpe/level/tileentity/BeaconTileEntity.h"
+//#include "mcpe/level/tileentity/BeaconTileEntity.h"
 
 class BeaconTile: public EntityTile {
 public:	
 	BeaconTile(int);
 
 	virtual const TextureUVCoordinateSet& getTexture(signed char, int);
-	virtual TileEntity* newTileEntity(const TilePos &);
+	//virtual TileEntity* newTileEntity(const TilePos &);
 	
 private:
 	TextureUVCoordinateSet secondary_tex;
+	TextureUVCoordinateSet terciary_tex;
+	TextureUVCoordinateSet fourty_tex;
+	TextureUVCoordinateSet fifty_tex;
 };
